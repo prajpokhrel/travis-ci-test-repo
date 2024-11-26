96fd35cd98490bd9ed1649af95de3ce1196a304b
Prajwal Pokhrel <prajwalpokhrel@prajwals-air-2.home>
2024-11-26 15:45:32 -0500

    feat: add firebase deploy after build success

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeEOaUeyxCgeSjhiSSE1gD2CrinLElOgE",
+  authDomain: "travis-test-d1396.firebaseapp.com",
+  projectId: "travis-test-d1396",
+  storageBucket: "travis-test-d1396.firebasestorage.app",
+  messagingSenderId: "983183025486",
+  appId: "1:983183025486:web:908e43355cd70ca02dea1f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
